cb4b1b8c846cfa80f37fe797991c07724f46a320
jltimm <jacobltimm@gmail.com>
2018-06-02 13:38:23 -0500

    Made the image data easier to read

--- ima.js
+++ ima.js
@@ -5,14 +5,23 @@ function readImage(fileName)
 {
     getPixels(fileName, function(err, pixels)
     {
+        var rgbaArray = [];
         if (err)
         {
             console.log("Bad image path");
             return;
         }
-        console.log("Got pixels", pixels.shape.slice());
-        console.log(pixels);
-    });    
+        var nx = pixels.shape[0];
+        var ny = pixels.shape[1];
+        for (var i = 0; i < nx; i++)
+        {
+            for (var j = 0; j < ny; j++)
+            {
+                rgbaArray.push({r: pixels.get(i, j, 0), g: pixels.get(i, j, 1), b: pixels.get(i, j, 2), a: pixels.get(i, j, 3)});
+            }
+        }
+        console.log(rgbaArray);
+    });
 }
 
 function main()
