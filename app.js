36d7f50a7fd557cdbba27033acce5386afe7789d
jltimm <jacobltimm@gmail.com>
2018-06-02 13:07:20 -0500

    Added getPixels dependency

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var msg = 'Hello world.';
-console.log(msg);
+var fs = require('fs');
+var getPixels = require('get-pixels');
+
+function readImage(fileName)
+{
+    getPixels(fileName, function(err, pixels)
+    {
+        if (err)
+        {
+            console.log("Bad image path");
+            return;
+        }
+        console.log("Got pixels", pixels.shape.slice());
+        console.log(pixels);
+    });    
+}
+
+function main()
+{
+    readImage("test_images/test-image.png");
+}
+
+main();
