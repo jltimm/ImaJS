fb09b13f6ef9795f819e206e8f3061cbe384f14d
jltimm <jacobltimm@gmail.com>
2018-06-24 12:33:58 -0500

    Created function convertToBlackAndWhite, got rid of readImage

--- ima.js
+++ ima.js
@@ -1,41 +1,53 @@
 var fs = require('fs');
 var getPixels = require('get-pixels');
 
-function convertToBlackAndWhite(rgbaArray)
-{
-
-}
-
 /**
- * Reads an image into the RGB pixel format
- * @param {the name of the file} fileName 
+ * Converts an image to black and white, and either writes to a file or returns an array
+ * @param {string} fileName The name of the file to be read
+ * @param {array} rgbaArray The array of pixels to be read
+ * @param {boolean} shouldWriteToFile Boolean representing if the file should be written
+ * @param {function} callback The callback
  */
-function readImage(fileName, callback)
+function convertToBlackAndWhite(fileName, callback)
 {
-    getPixels(fileName, function(err, pixels)
+    if (fileName)
     {
-        var rgbaArray = [];
-        if (err)
+        getPixels(fileName, function(err, pixels)
         {
-            console.log("Bad image path");
-            return;
-        }
-        var nx = pixels.shape[0];
-        var ny = pixels.shape[1];
-        for (var i = 0; i < nx; i++)
-        {
-            for (var j = 0; j < ny; j++)
+            if (err)
             {
-                rgbaArray.push({r: pixels.get(i, j, 0), g: pixels.get(i, j, 1), b: pixels.get(i, j, 2), a: pixels.get(i, j, 3)});
+                callback(new Error("Error occurred while getting pixels", null));
             }
-        }
-        callback(null, rgbaArray);
-    });
+
+            var bwArray = [];
+            var nx = pixels.shape[0];
+            var ny = pixels.shape[1];
+
+            for (var i = 0; i < nx; i++)
+            {
+                for (var j = 0; j < ny; j++)
+                {
+                    bwArray.push(
+                    {
+                        r: pixels.get(i, j, 0) * 0.299,
+                        g: pixels.get(i, j, 1) * 0.587,
+                        b: pixels.get(i, j, 2) * 0.114,
+                        a: pixels.get(i, j, 3)
+                    });
+                }
+            }
+            
+            callback(null, bwArray);
+        });
+    } else
+    {
+        callback(new Error("Filename is null."), null);
+    }
 }
 
 function main()
 {
-    readImage("test_images/test-image.png", function(err, data)
+    convertToBlackAndWhite("test_images/test-image.png", function(err, data)
     {
         if (err) throw err;
         console.log(data);
