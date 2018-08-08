284ae68d588cf8ad82eebf604cd881a15b5f9d22
jltimm <jacobltimm@gmail.com>
2018-08-07 19:36:28 -0500

    Style fixes

--- ima.js
+++ ima.js
@@ -3,65 +3,14 @@ var path = require('path');
 var PNG = require('pngjs').PNG;
 
 /**
- * Converts an image to sepia and returns an RGBA array
- * @param {string} fileName The name of the file to be read
- * @param {function} callback The callback
- */
-function convertToSepia(fileName, callback)
-{
-    if (fileName)
-    {
-        getPixels(fileName, function(err, pixels)
-        {
-            if (err)
-            {
-                callback(new Error("Error occurred while getting pixels", null));
-            }
-
-            var sepiaArray = [];
-            var nx = pixels[0].length;
-            var ny = pixels.length;
-
-            for (var i = 0; i < nx; i++)
-            {
-                sepiaRow = [];
-                for (var j = 0; j < ny; j++)
-                {
-                    var r = (pixels[i][j][0] * .393) + (pixels[i][j][1] * .769) + (pixels[i][j][2] * .189);
-                    var g = (pixels[i][j][0] * .349) + (pixels[i][j][1] * .686) + (pixels[i][j][2] * .168);
-                    var b = (pixels[i][j][0] * .272) + (pixels[i][j][1] * .534) + (pixels[i][j][2] * .131);
-                    sepiaRow.push(
-                    {
-                        r: (r > 255 ? 255 : r),
-                        g: (g > 255 ? 255 : g),
-                        b: (b > 255 ? 255 : b),
-                        a: pixels[i][j][3]
-                    });
-                }
-                sepiaArray.push(sepiaRow);
-            }
-
-            callback(null, sepiaArray);
-        });
-    } else
-    {
-        callback(new Error("Filename is null"));
-    }
-}
-
-/**
  * Converts an image to black and white and returns an RGBA array
  * @param {string} fileName The name of the file to be read
  * @param {function} callback The callback
  */
-function convertToBlackAndWhite(fileName, callback)
-{
-    if (fileName)
-    {
-        getPixels(fileName, function(err, pixels)
-        {
-            if (err)
-            {
+function convertToBlackAndWhite(fileName, callback) {
+    if (fileName) {
+        getPixels(fileName, function(err, pixels) {
+            if (err) {
                 callback(new Error("Error occurred while getting pixels", null));
             }
 
@@ -69,13 +18,10 @@ function convertToBlackAndWhite(fileName, callback)
             var nx = pixels[0].length;
             var ny = pixels.length;
 
-            for (var i = 0; i < nx; i++)
-            {
+            for (var i = 0; i < nx; i++) {
                 bwRow = [];
-                for (var j = 0; j < ny; j++)
-                {
-                    bwRow.push(
-                    {
+                for (var j = 0; j < ny; j++) {
+                    bwRow.push({
                         r: pixels[i][j][0] * 0.299,
                         g: pixels[i][j][1] * 0.587,
                         b: pixels[i][j][2] * 0.114,
@@ -87,8 +33,7 @@ function convertToBlackAndWhite(fileName, callback)
 
             callback(null, bwArray);
         });
-    } else
-    {
+    } else {
         callback(new Error("Filename is null."), null);
     }
 }
@@ -98,20 +43,16 @@ function convertToBlackAndWhite(fileName, callback)
  * @param {string} fileName The filename
  * @param {function} callback The callback function 
  */
-function getPixels(fileName, callback)
-{
+function getPixels(fileName, callback) {
     var data = fs.readFileSync('test_images/test-image.png');
     var png = new PNG();
-    png.parse(data, function(err, img_data)
-    {
+    png.parse(data, function(err, img_data) {
         origArray = new Uint8Array(img_data.data);
         rgbaArray = [];
         row = [];
-        for (i = 0; i < (png.width * png.height * 4); i+=4)
-        {
+        for (i = 0; i < (png.width * png.height * 4); i+=4) {
             row.push([origArray[i], origArray[i+1], origArray[i+2], origArray[i+3]]);
-            if (row.length == png.width)
-            {
+            if (row.length == png.width) {
                 rgbaArray.push(row);
                 row = [];
             }
@@ -126,13 +67,10 @@ function getPixels(fileName, callback)
  * @param {2d array} data The pixel data 
  * @param {function} callback The callback 
  */
-function writeToFile(fileName, newFileName, data, callback)
-{
+function writeToFile(fileName, newFileName, data, callback) {
     buffer = []
-    for (i = 0; i < data[0].length; i++)
-    {
-        for (j = 0; j < data.length; j++)
-        {
+    for (i = 0; i < data[0].length; i++) {
+        for (j = 0; j < data.length; j++) {
             buffer.push(Math.round(data[i][j].r));
             buffer.push(Math.round(data[i][j].g));
             buffer.push(Math.round(data[i][j].b));
@@ -147,11 +85,9 @@ function writeToFile(fileName, newFileName, data, callback)
 
 function main()
 {
-    convertToSepia("test_images/test-image.png", function(err, data)
-    {
+    convertToBlackAndWhite("test_images/test-image.png", function(err, data) {
         if (err) throw err;
-        writeToFile("test.png", "testout.png", data, function(err)
-        {
+        writeToFile("test.png", "testout.png", data, function(err) {
             if (err) throw err;
         });
     });
