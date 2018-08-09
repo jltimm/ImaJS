24c594066f1ef01f9ac1908c98beaa178e780e33
jltimm <jacobltimm@gmail.com>
2018-08-08 19:15:54 -0500

    Added sobel operator

--- ima.js
+++ ima.js
@@ -19,6 +19,7 @@ ImaJS.prototype.grayscale = function(filename, callback) {
             var grayscaleArray = [];
             var nx = pixels[0].length;
             var ny = pixels.length;
+            // TODO: 3 two arrays simultaneously: original, grayscale, edge
             for (var i = 0; i < nx; i++) {
                 var grayscaleRow = [];
                 for (var j = 0; j < ny; j++) {
@@ -35,6 +36,56 @@ ImaJS.prototype.grayscale = function(filename, callback) {
     } else {
         callback(new Error("Filename is null."), null);
     }
+}
+
+/**
+ * Applies sobel filter to an image
+ * @param {2d array} data The data
+ * @param {function} callback The callback
+ */
+ImaJS.prototype.sobel = function(image) {
+    var kernelX = [
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0, -1]
+    ];
+    var kernelY = [
+        [ 1,  2,  1],
+        [ 0,  0,  0],
+        [-1, -2, -1]
+    ];
+    var sobelArray = [];
+    for (var x = 1; x < image.length-1; x++) {
+        var sobelRow = [];
+        for (var y = 1; y < image[0].length-1; y++) {
+            var g = calculateGradients(image, x, y, kernelX, kernelY);
+            sobelRow.push(g);
+        }
+        sobelArray.push(sobelRow);
+    }
+    return sobelArray;
+}
+
+/**
+ * Calculates the gradients according to the kernels.
+ * @param {2d array} image The array of pixels
+ * @param {int} x The current x position
+ * @param {int} y The current y position
+ * @param {2d array} kernelX The x kernel
+ * @param {2d array} kernelY The y kernel
+ */
+function calculateGradients(image, x, y, kernelX, kernelY) {
+	var gx = (kernelX[2][2] * image[x-1][y-1]) + (kernelX[2][1] * image[x-1][y]) + (kernelX[2][0] * image[x-1][y+1]) +
+             (kernelX[1][2] * image[x][y-1]) + (kernelX[1][1] * image[x][y]) + (kernelX[1][0] * image[x][y+1]) +
+             (kernelX[0][2] * image[x+1][y-1]) + (kernelX[0][1] * image[x+1][y]) + (kernelX[0][0] * image[x+1][y+1]);
+    var gy = (kernelY[2][2] * image[x-1][y-1]) + (kernelY[2][1] * image[x-1][y]) + (kernelY[2][0] * image[x-1][y+1]) +
+             (kernelY[1][2] * image[x][y-1]) + (kernelY[1][1] * image[x][y]) + (kernelY[1][0] * image[x][y+1]) +
+             (kernelY[0][2] * image[x+1][y-1]) + (kernelY[0][1] * image[x+1][y]) + (kernelY[0][0] * image[x+1][y+1]);
+    var g = Math.round(Math.sqrt((gx * gx) + (gy * gy)));
+    if (g > 255) {
+        return 255;
+    }
+    return g;
 }
 
 /**
--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 var ImaJS = require('../ima.js');
 
 function runTest() {
-  var ima = new ImaJS();
-  ima.grayscale('./test_images/test-image.png', function(err, pixelArray) {
-    if (err) throw err;
-    ima.writeFileSync('testout.png', pixelArray);
-  });
+	var ima = new ImaJS();
+  	ima.grayscale('./test_images/test-image.png', function(err, pixelArray) {
+    	if (err) throw err;
+		var sobel = ima.sobel(pixelArray);
+    	ima.writeFileSync('testout.png', sobel);
+  	});
 }
 
 runTest();
